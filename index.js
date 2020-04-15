736f11212430cb1bce5ece4013d5b3529459dbd5
twoherohon1 <twoherohon1@hotmail.com>
2020-04-15 19:30:50 +0700

    test

---
+++ index.js
@@ -0,0 +1,14 @@
+let express = require('express')
+let app = express()
+
+app.get('/',(req,res)=>{
+    console.log('Hello World')
+    res.send('Hello World')
+})
+
+app.get('/crash',(req,res)=>{
+    console.log('Crash!!')
+    res.send('Crash!!')
+    process.exit(1)
+})
+app.listen(process.env.PORT,()=>console.log('Sever is Running at',process.env.PORT))
